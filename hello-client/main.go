c99b615292da0c31f0b6584771375f3125ce0a3e
duanyaowei <1978756405@qq.com>
2023-07-26 13:49:24 +0800

    新增：DuanTest 自己写了另一个测试gprc接口方法

--- hello-client/main.go
+++ hello-client/main.go
@@ -11,22 +11,31 @@ import (
 )
 
 func main() {
-	start := time.Now() // 记录开始时间
+	//start := time.Now() // 记录开始时间
+	//cc, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//if err != nil {
+	//	log.Fatalf("did not connect: %v", err)
+	//}
+	//defer cc.Close()
+	//// 建立连接
+	//client := pb.NewSayHelloClient(cc)
+	//// 执行 rpc 调用（这个方法在服务器端来实现并返回结果）
+	//resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "kuangshen"})
+	//fmt.Println(resp.GetResponseMsg())
+	//elapsed := time.Since(start) // 计算经过的时间
+	//fmt.Println("程序运行耗时:", elapsed)
 
-	cc, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	start := time.Now() // 记录开始时间
+	cc, err := grpc.Dial("127.0.0.1:9001", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer cc.Close()
-
 	// 建立连接
-	client := pb.NewSayHelloClient(cc)
-
+	client := pb.NewDuanTestClient(cc)
 	// 执行 rpc 调用（这个方法在服务器端来实现并返回结果）
-	resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "kuangshen"})
-
-	fmt.Println(resp.GetResponseMsg())
-
+	resp, _ := client.DuanTest(context.Background(), &pb.DuanParams{Name: "KD", Age: 23, Address: "美国超音速"})
+	fmt.Println(resp.GetResponseCode())
 	elapsed := time.Since(start) // 计算经过的时间
 	fmt.Println("程序运行耗时:", elapsed)
 }
--- hello-server/main.go
+++ hello-server/main.go
@@ -17,17 +17,42 @@ func (s server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloResponse{ResponseMsg: "hello" + req.RequestName}, nil
 }
 
+type DuanSerer struct {
+	pb.UnimplementedDuanTestServer
+}
+
+func (d DuanSerer) DuanTest(ctx context.Context, req *pb.DuanParams) (*pb.DuanResponse, error) {
+	var duan = &pb.DuanParams{}
+	duan.Name = req.Name
+	duan.Age = req.Age
+	duan.Address = req.Address
+
+	return &pb.DuanResponse{ResponseMsg: "响应成功", ResponseCode: 200}, nil
+}
+
 func main() {
-	// 开启端口
-	listen, _ := net.Listen("tcp", ":9090")
-	// 创建 grpc 服务
-	grpcServer := grpc.NewServer()
-	// 在 grpc 服务中注册我们自己编写的服务
-	pb.RegisterSayHelloServer(grpcServer, &server{})
-	// 启动服务
-	err := grpcServer.Serve(listen)
+
+	//// 开启端口
+	//listen, _ := net.Listen("tcp", ":9090")
+	//// 创建 grpc 服务
+	//grpcServer := grpc.NewServer()
+	//// 在 grpc 服务中注册我们自己编写的服务
+	//pb.RegisterSayHelloServer(grpcServer, &server{})
+	//// 启动服务
+	//err := grpcServer.Serve(listen)
+	//if err != nil {
+	//	fmt.Printf("failed to serve: %v", err)
+	//	return
+	//}
+
+	//--------------------------duan test
+	listen, _ := net.Listen("tcp", ":9001") //开启端口
+	grpcServer := grpc.NewServer()          //创建grpc服务
+	pb.RegisterDuanTestServer(grpcServer, &DuanSerer{})
+	err := grpcServer.Serve(listen) //启动服务
 	if err != nil {
-		fmt.Printf("failed to serve: %v", err)
+		fmt.Printf("failed to serve: #{err}")
 		return
 	}
+
 }
