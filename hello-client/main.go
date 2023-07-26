a6c09d4d67a44da29603d3d678df21363d4cba1c
duanyaowei <1978756405@qq.com>
2023-07-26 14:31:22 +0800

    新增： grpc-token认证代码

--- hello-client/main.go
+++ hello-client/main.go
@@ -10,21 +10,41 @@ import (
 	"time"
 )
 
-func main() {
-	//start := time.Now() // 记录开始时间
-	//cc, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	//if err != nil {
-	//	log.Fatalf("did not connect: %v", err)
-	//}
-	//defer cc.Close()
-	//// 建立连接
-	//client := pb.NewSayHelloClient(cc)
-	//// 执行 rpc 调用（这个方法在服务器端来实现并返回结果）
-	//resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "kuangshen"})
-	//fmt.Println(resp.GetResponseMsg())
-	//elapsed := time.Since(start) // 计算经过的时间
-	//fmt.Println("程序运行耗时:", elapsed)
+type ClientTokenAuth struct {
+}
 
+func (c ClientTokenAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appId":  "kuangshen",
+		"appKey": "123123",
+	}, nil
+}
+
+func (c ClientTokenAuth) RequireTransportSecurity() bool {
+	return false
+}
+
+// -------------------初始化测试--------------
+
+func ServerHello() {
+	start := time.Now() // 记录开始时间
+	cc, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer cc.Close()
+	// 建立连接
+	client := pb.NewSayHelloClient(cc)
+	// 执行 rpc 调用（这个方法在服务器端来实现并返回结果）
+	resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "kuangshen"})
+	fmt.Println(resp.GetResponseMsg())
+	elapsed := time.Since(start) // 计算经过的时间
+	fmt.Println("程序运行耗时:", elapsed)
+}
+
+// -------------------新增测试--------------
+
+func DuanTest() {
 	start := time.Now() // 记录开始时间
 	cc, err := grpc.Dial("127.0.0.1:9001", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -38,4 +58,34 @@ func main() {
 	fmt.Println(resp.GetResponseCode())
 	elapsed := time.Since(start) // 计算经过的时间
 	fmt.Println("程序运行耗时:", elapsed)
+}
+
+// -------------------token认证--------------
+
+func AuthToken() {
+	start := time.Now() // 记录开始时间
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithPerRPCCredentials(new(ClientTokenAuth)))
+
+	cc, err := grpc.Dial("127.0.0.1:9002", opts...)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer cc.Close()
+
+	// 建立连接
+	client := pb.NewTokenHelloClient(cc)
+	// 执行 rpc 调用（这个方法在服务器端来实现并返回结果）
+	resp, _ := client.TokenHello(context.Background(), &pb.TokenRequest{RequestName: "kuangshen"})
+	fmt.Println(resp.GetResponseMsg())
+
+	elapsed := time.Since(start) // 计算经过的时间
+	fmt.Println("程序运行耗时:", elapsed)
+
+}
+
+func main() {
+	AuthToken()
 }
--- hello-server/main.go
+++ hello-server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"net"
 	pb "stay_grpc/hello-server/proto"
 )
@@ -30,22 +33,54 @@ func (d DuanSerer) DuanTest(ctx context.Context, req *pb.DuanParams) (*pb.DuanRe
 	return &pb.DuanResponse{ResponseMsg: "响应成功", ResponseCode: 200}, nil
 }
 
-func main() {
+type TokenServer struct {
+	pb.UnimplementedTokenHelloServer
+}
 
-	//// 开启端口
-	//listen, _ := net.Listen("tcp", ":9090")
-	//// 创建 grpc 服务
-	//grpcServer := grpc.NewServer()
-	//// 在 grpc 服务中注册我们自己编写的服务
-	//pb.RegisterSayHelloServer(grpcServer, &server{})
-	//// 启动服务
-	//err := grpcServer.Serve(listen)
-	//if err != nil {
-	//	fmt.Printf("failed to serve: %v", err)
-	//	return
-	//}
+func (t TokenServer) TokenHello(ctx context.Context, req *pb.TokenRequest) (*pb.TokenResponse, error) {
+	// 获取元数据信息
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("未传输 token")
+	}
+	var appId string
+	var appKey string
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+		fmt.Println("appID:", appId)
+	}
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+		fmt.Println("appKey:", appKey)
+	}
+	if appId != "kuangshen" || appKey != "123123" {
+		return nil, errors.New("token 不正确")
+	}
 
-	//--------------------------duan test
+	fmt.Printf("hello" + req.RequestName)
+	return &pb.TokenResponse{ResponseMsg: "hello" + req.RequestName}, nil
+}
+
+// -------------------------------初始生成测试-------------------------------
+
+func ServerSayHello() {
+	// 开启端口
+	listen, _ := net.Listen("tcp", ":9090")
+	// 创建 grpc 服务
+	grpcServer := grpc.NewServer()
+	// 在 grpc 服务中注册我们自己编写的服务
+	pb.RegisterSayHelloServer(grpcServer, &server{})
+	// 启动服务
+	err := grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Printf("failed to serve: %v", err)
+		return
+	}
+}
+
+// -------------------------------新增测试-------------------------------
+
+func DuanServerTest() {
 	listen, _ := net.Listen("tcp", ":9001") //开启端口
 	grpcServer := grpc.NewServer()          //创建grpc服务
 	pb.RegisterDuanTestServer(grpcServer, &DuanSerer{})
@@ -54,5 +89,31 @@ func main() {
 		fmt.Printf("failed to serve: #{err}")
 		return
 	}
+}
 
+// ----------token认证 ：gRPC 提供了一个接口，位于credentials包下，需要客户端来实现-----
+
+// type PerRPCCredentials interface {
+//	GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error)
+//	RequireTransportSecurity() bool
+//}
+
+func AuthToken() {
+	// 开启端口
+	listen, _ := net.Listen("tcp", ":9002")
+	// 创建 grpc 服务
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	// 在 grpc 服务中注册我们自己编写的服务
+	pb.RegisterTokenHelloServer(grpcServer, &TokenServer{})
+	// 启动服务
+	err := grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Printf("failed to serve: %v", err)
+		return
+	}
+
+}
+
+func main() {
+	AuthToken()
 }
